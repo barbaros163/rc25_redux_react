6e3f42eb8f2dd1e11c92fbd7373405e4b621d5d8
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-11 21:55:07 +0100

    start

--- redux-intro-inClass/src/redux/actions/todoAction.js
+++
@@ -1,9 +0,0 @@
-import { ADD_TODO, CLEAR_TODO } from "../types/todoTypes";
-
-export const addTodo = (payload) => {
-  return { type: ADD_TODO, payload: payload };
-};
-
-export const clearTodo = () => {
-  return { type: CLEAR_TODO };
-};
--- redux-intro-inClass/src/redux/index.jsx
+++ redux-intro-inClass/src/redux/index.jsx
@@ -16,13 +16,25 @@
 // };
 // export default reducer;
 
-import { legacy_createStore as createStore, combineReducers } from "redux";
-import counterReducer from "./reducers/counterReducer";
-import todoReducer from "./reducers/todoReducer";
+const initialState = {
+  counter: 0,
+};
 
-const rootReducer = combineReducers({
-  count: counterReducer,
-  todo: todoReducer,
-});
+const reducer = (state: initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { counter: state.counter + 1 };
 
-export const store = createStore(rootReducer);
+    case "DECREMENT":
+      return { counter: state.counter - 1 };
+
+    case "CLEAR":
+      // return {counter: 0}
+      return initialState;
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
